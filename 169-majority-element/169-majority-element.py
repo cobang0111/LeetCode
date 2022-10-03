5d8294ec5c1144a6d76ae91b81c794508901cf0a
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2022-10-04 07:47:09 +0900

    Time: 307 ms (48.69%), Space: 15.6 MB (34.06%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        for i in set(nums):
+            if nums.count(i)>len(nums)/2:
+                break
+        
+        return i
