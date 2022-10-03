f3aefc95ad1004b5c6cfa12b3f2eada0f7bda4fe
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2022-10-04 07:46:55 +0900

    Delete 169-majority-element directory

--- 169-majority-element/169-majority-element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-
-        for i in set(nums):
-            if nums.count(i)>len(nums)/2:
-                break
-        
-        return i
