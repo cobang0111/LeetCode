996f5d0b7721616e9559425721bddd22f95bfe96
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2022-10-04 07:46:12 +0900

    Time: 468 ms (7.62%), Space: 15.5 MB (34.06%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,17 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        count=0
-        number=nums[0]
 
-        for i in nums:
-            if number==i:
-                count+=1
-            else:
-                number=i
-                count=1
-    
-            if count>(len(nums)/2):
+        for i in set(nums):
+            if nums.count(i)>len(nums)/2:
                 break
         
-        return number
+        return i
