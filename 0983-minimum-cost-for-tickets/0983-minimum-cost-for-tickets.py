bdeae0a6de69708c4ffa8e615fb5e178ce95b052
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-03-29 01:03:39 +0900

    Time: 55 ms (33.68%), Space: 13.9 MB (83.75%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,22 +1,23 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        travel_cal = []
-        total_charge = []
+        travel_cal = [] #Record Travel day
+        total_charge = [] #Dynamic Programming - Record minimum cost for each day 
 
-
+        #Making list
         for i in range(1, days[-1]+1):
             total_charge.append(0)
+            #Record Travel day as 1, and if not, then 0
             if i in days:
                 travel_cal.append(1)
             else:
                 travel_cal.append(0)
-
+        
         i=0
-
+        #Case for index out of range
         if travel_cal[0] == 0:
             total_charge[0] = 0
             i+=1
-
+        #Loop : Making minimum cost list for each day
         while i < len(travel_cal):
             if travel_cal[i] != 1:
                 total_charge[i] = total_charge[i-1]
@@ -27,10 +28,8 @@ class Solution:
                     total_charge[i]+=min(total_charge[i-1]+costs[0], total_charge[i-7]+costs[1], costs[2])
                 else:
                     total_charge[i]+=min(total_charge[i-1]+costs[0], costs[1], costs[2])
-
             i+=1
-
-
+        #Result
         return total_charge[-1]   
 
                 
