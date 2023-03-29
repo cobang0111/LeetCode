d5e4a6cfde9d5d61434cc6b8b704740d2b91d6fd
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-03-29 15:59:54 +0900

    Time: 46 ms (58.23%), Space: 14 MB (25.00%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -5,8 +5,7 @@ class Solution:
         max_dish = 0
         temp = 0
         for i in range(time):
-            temp += sum(satisfaction[-i-1:])
+            temp += sum(satisfaction[time-1-i:])
             if temp > max_dish:
                 max_dish = temp
-            
         return max_dish
