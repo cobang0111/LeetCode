b071ebbea02a113c5ff9401173e6ac953ae70ee5
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-04-11 11:39:18 +0900

    Time: 4270 ms (5.07%), Space: 15.3 MB (95.97%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -6,8 +6,5 @@ class Solution:
                 break
             star_index = s.index('*')
             cur_pos = star_index - 1
-            if cur_pos == '*' or cur_pos == '':
-                while cur_pos == '*' or cur_pos == '':
-                    cur_pos -= 1
             s = s[:cur_pos] + s[star_index+1:]
         return s
